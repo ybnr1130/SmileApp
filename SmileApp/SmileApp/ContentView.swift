475118ce9bd30a128e686134d41759444c9342af
ybnr1130 <muuckk@gmail.com>
2021-12-27 14:35:08 +0900

    Finish making smiley image

--- SmileApp/SmileApp/ContentView.swift
+++ SmileApp/SmileApp/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName:"smiley")
+            .resizable()
+            .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
     }
 }
 
