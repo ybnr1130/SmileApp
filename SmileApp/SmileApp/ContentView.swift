2fb3ee2c0a11baa04889bf94fc2be500f4dc4491
ybnr1130 <muuckk@gmail.com>
2021-12-27 14:26:16 +0900

    First commit

---
+++ SmileApp/SmileApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmileApp
+//
+//  Created by 薮内紀仁 on 2021/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmileApp/SmileApp/SmileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileAppApp.swift
+//  SmileApp
+//
+//  Created by 薮内紀仁 on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct SmileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
